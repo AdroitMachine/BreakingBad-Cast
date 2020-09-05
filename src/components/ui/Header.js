ec44ce8de94b51c9a149ff23bd405be6d5f5a83f
AdroitMachine <anujsaxenamail@gmail.com>
2020-09-05 19:43:10 +0530

    initial commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="center">
+        <img src={logo} alt="" />
+      </header>
+    </div>
+  );
+};
+
+export default Header;
